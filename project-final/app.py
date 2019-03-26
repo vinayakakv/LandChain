e0b3ad24cbc1d4265541989350a5901c3a161501
vinayaka <me.vinayakakv@gmail.com>
2019-03-26 12:00:33 +0530

    Moved survey records to the Blockchain

--- project-final/app.py
+++ project-final/app.py
@@ -1,21 +1,25 @@
+import json
+
 from flask import Flask, render_template, send_from_directory, request, jsonify
+
 from cryptoland.land_operations import Survey
 from cryptoland.user_config import UserConfig
-import json
 
 app = Flask(__name__, static_url_path='')
 user = UserConfig()
 
 
 @app.route('/<path:outerdir>/<path:path>')
-def send_file(outerdir,path):
+def send_file(outerdir, path):
     return send_from_directory('templates/', outerdir + '/' + path + '.html')
 
 
 @app.route('/saveSurvey', methods=['POST'])
 def save_survey():
+    if user.get_user_type() != "SURVEYOR":
+        return jsonify({"success": False, "message": "Only Surveyor can survey"})
     s = Survey(request.data)
-    return jsonify({"success": True, "data": s.__dict__})
+    return jsonify({"success": True, "data": json.loads(str(s))})
 
 
 @app.route('/getSurveys', methods=['POST', 'GET'])
--- project-final/cryptoland/land_operations.py
+++ project-final/cryptoland/land_operations.py
@@ -1,27 +1,63 @@
 import json
 
+from bigchaindb_driver.crypto import CryptoKeypair
+
+from .user_config import TransactionHelper
+from .user_config import UserConfig, GOVERNMENT_PUBKEY
+
+user = UserConfig()
+
 
 class Survey:
     def __init__(self, request):
-        # self.raw_data = request
+        self.transactionHelper = TransactionHelper("http://bigchaindb:9984")
         request = json.loads(request)
         self.surveyNumber = request['surveyNumber']
         self.landType = request['landType']
         self.boundaries = json.loads(request['boundaries'])
         self.id = self.boundaries["id"]
+        self.area = int(request["area"])
+        self.type = "SURVEY"
         self.save()
 
     def __str__(self):
-        return json.dumps(self.__dict__)
+        dictionary = {
+            "surveyNumber": self.surveyNumber,
+            "boundaries": self.boundaries,
+            "landType": self.landType,
+            "id": self.id,
+            "type": self.type
+        }
+        return json.dumps(dictionary)
 
     def save(self):
-        with open("surveys.txt", "a")as f:
-            f.write(str(self) + "\n")
+        asset = {
+            'data': {
+                "surveyNumber": self.surveyNumber,
+                "boundaries": json.dumps(self.boundaries),
+                "landType": self.landType,
+                "id": self.id,
+                "type": self.type
+            }
+        }
+        current_user = user.user
+        keypair = CryptoKeypair(public_key=current_user['pub.key'],
+                                private_key=current_user['priv.key'])
+        print(asset, keypair)
+        self.transactionHelper.create_divisible_asset(
+            creator=keypair,
+            owner_pubkey=GOVERNMENT_PUBKEY,
+            asset=asset,
+            quantity=self.area
+        )
 
     @staticmethod
     def get_surveys():
-        try:
-            with open("surveys.txt", "r+") as f:
-                return [json.loads(x) for x in f.readlines() if x != " "]
-        except IOError:
-            return []
+        transactionHelper = TransactionHelper("http://bigchaindb:9984")
+        data = transactionHelper.find_asset("SURVEY")
+        results = []
+        for asset in data:
+            asset = asset['data']
+            asset['boundaries'] = json.loads(asset['boundaries'])
+            results.append(asset)
+        return results
